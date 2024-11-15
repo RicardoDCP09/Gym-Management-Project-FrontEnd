db47ea1944d9c096915d362cfdc089d02276e361
Barragan <jcbarragan1994@gmail.com>
2024-11-15 19:50:14 -0400

    error membershio module, create .env file, update json-server file

--- src/helpers/helpFetch.js
+++ src/helpers/helpFetch.js
@@ -1,40 +1,40 @@
-export const helpFetch = (baseUrl) => {
+const baseUrl = import.meta.env.VITE_BACKEND_URL
 
-    const customFetch = async (url, method, body = null) => {
-        try {
-            const options = {
-                method: method,
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            };
-            if (body) {
-                options.body = JSON.stringify(body);
-            }
+export const helpFetch = () => {
+  const customFetch = async (url, method, body = null) => {
+    try {
+      const options = {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      if (body) {
+        options.body = JSON.stringify(body)
+      }
 
-            const response = await fetch(url, options);
+      const response = await fetch(url, options)
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
 
-            const data = await response.json();
-            return data;
-        }
-        catch (error) {
-            console.error(error);
-            return null;
-        }
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.error(error)
+      return null
     }
+  }
 
-    // Métodos 
-    const get = (endpoint) => customFetch(`${baseUrl}${endpoint}`, 'GET');
+  // Métodos
+  const get = (endpoint) => customFetch(`${baseUrl}${endpoint}`, 'GET')
 
-    const post = (endpoint, body) => customFetch(`${baseUrl}${endpoint}`, 'POST', body);
+  const post = (endpoint, body) => customFetch(`${baseUrl}${endpoint}`, 'POST', body)
 
-    const put = (endpoint, body, id) => customFetch(`${baseUrl}${endpoint}/${id}`, 'PUT', body);
+  const put = (endpoint, body, id) => customFetch(`${baseUrl}${endpoint}/${id}`, 'PUT', body)
 
-    const del = (endpoint, id) => customFetch(`${baseUrl}${endpoint}/${id}`, 'DELETE');
+  const del = (endpoint, id) => customFetch(`${baseUrl}${endpoint}/${id}`, 'DELETE')
 
-    return { get, post, put, del };
+  return { get, post, put, del, customFetch }
 }
--- src/views/pages/membership/membership.js
+++ src/views/pages/membership/membership.js
@@ -1,10 +1,5 @@
 import CIcon from '@coreui/icons-react'
-import {
-  cilClock,
-  cilTag,
-  cilColorBorder,
-  cilCash,
-} from '@coreui/icons'
+import { cilClock, cilTag, cilColorBorder, cilCash } from '@coreui/icons'
 
 import {
   CCard,
@@ -26,72 +21,84 @@ import {
   CModalBody,
   CModalFooter,
   CModalTitle,
-
-} from '@coreui/react';
+} from '@coreui/react'
 import { useState, useEffect } from 'react'
-import { helpFetch } from '../../../helpers/helpFetch.js';
+import { helpFetch } from '../../../helpers/helpFetch.js'
 const Membership = () => {
-  const API = helpFetch('http://localhost:5000/');
-  const [memberships, setMemberships] = useState([]);
+  const API = helpFetch()
+  const [memberships, setMemberships] = useState([])
   const [visibleEdit, setVisibleEdit] = useState(false)
   const [visibleDelete, setVisibleDelete] = useState(false)
-  const [currentMembership, setCurrentMembership] = useState(null);
-  const [newMembership, setNewMembership] = useState({ name: '', duration: '', price: '' });
-  const [deleteConfirmation, setDeleteConfirmation] = useState('');
+  const [currentMembership, setCurrentMembership] = useState(null)
+  const [newMembership, setNewMembership] = useState({ name: '', duration: '', price: '' })
+  const [deleteConfirmation, setDeleteConfirmation] = useState('')
 
   useEffect(() => {
     const fetchMemberships = async () => {
-      const data = await API.get('type_memberships');
-      setMemberships(data);
-    };
-    fetchMemberships();
-  }, [API]);
-
+      const data = await API.get('type_memberships')
+      setMemberships(data)
+    }
+    fetchMemberships()
+  }, [])
 
   const handleAddMembership = async () => {
-    const addedMembership = await API.post('type_memberships', newMembership);
-    setMemberships([...memberships, addedMembership]);
-    setNewMembership({ name: '', duration: '', price: '' });
-  };
+    const addedMembership = await API.post('type_memberships', newMembership)
+    setMemberships([...memberships, addedMembership])
+    setNewMembership({ name: '', duration: '', price: '' })
+  }
 
   const handleEditMembership = async () => {
     if (!currentMembership || !currentMembership.id) {
-      console.error("Current membership is not set or does not have an ID.");
-      return;
+      console.error('Current membership is not set or does not have an ID.')
+      return
     }
-
-    console.log("Editing membership:", currentMembership);
 
     try {
-      const updatedMembership = await API.put(`type_memberships/${currentMembership.id}`, currentMembership);
-      setMemberships(memberships.map((membership) => membership.id === currentMembership.id ? updatedMembership : membership));
-      setVisibleEdit(false);
-    } catch (error) {
-      console.error("Error updating membership:", error);
-    }
-  };
+      const updatedMembership = await API.put(
+        'type_memberships',
+        currentMembership,
+        currentMembership.id,
+      )
 
+      setMemberships((prevMemberships) =>
+        prevMemberships.map((membership) =>
+          membership.id === currentMembership.id
+            ? { ...membership, ...updatedMembership }
+            : membership,
+        ),
+      )
+      setVisibleEdit(false)
+    } catch (error) {
+      console.error('Error updating membership:', error)
+    }
+  }
 
   const handleDeleteMembership = async () => {
     if (deleteConfirmation === 'confirm') {
-      const membershipId = currentMembership.id;
+      const membershipId = currentMembership.id
       try {
-        const deletedMembership = await API.del('type_memberships', membershipId);
-        setMemberships(memberships.filter((membership) => membership.id !== membershipId));
-        setVisibleDelete(false);
+        const deletedMembership = await API.del('type_memberships', membershipId)
+        setMemberships(memberships.filter((membership) => membership.id !== membershipId))
+        setVisibleDelete(false)
       } catch (error) {
-        console.error("Error deleting membership:", error);
+        console.error('Error deleting membership:', error)
       }
     }
-  };
+  }
 
   return (
-    < CCard className="mb-4" >
+    <CCard className="mb-4">
       <CCardHeader>
         <h4 className="mb-0">Membership Management</h4>
       </CCardHeader>
       <CCardBody>
-        <CForm className="mb-4" onSubmit={(e) => { e.preventDefault(); handleAddMembership(); }}>
+        <CForm
+          className="mb-4"
+          onSubmit={(e) => {
+            e.preventDefault()
+            handleAddMembership()
+          }}
+        >
           <CRow className="g-3">
             <CCol md={3}>
               <CFormInput
@@ -106,7 +113,6 @@ const Membership = () => {
                 type="number"
                 placeholder="Duration(days)"
                 value={newMembership.duration}
-
                 onChange={(e) => setNewMembership({ ...newMembership, duration: e.target.value })}
               />
             </CCol>
@@ -119,7 +125,7 @@ const Membership = () => {
               />
             </CCol>
             <CCol md={3}>
-              <CButton color="primary" type='submit'>
+              <CButton color="primary" type="submit">
                 Add
               </CButton>
             </CCol>
@@ -128,117 +134,157 @@ const Membership = () => {
         <CTable hover responsive>
           <CTableHead>
             <CTableRow>
-              <CTableHeaderCell>Name
+              <CTableHeaderCell>
+                Name
                 <CIcon icon={cilTag} customClassName="nav-icon icon-small" />
               </CTableHeaderCell>
-              <CTableHeaderCell>Duration (days)
+              <CTableHeaderCell>
+                Duration (days)
                 <CIcon icon={cilClock} customClassName="nav-icon icon-small" />
               </CTableHeaderCell>
-              <CTableHeaderCell>Price
+              <CTableHeaderCell>
+                Price
                 <CIcon icon={cilCash} customClassName="nav-icon icon-small" />
               </CTableHeaderCell>
-              <CTableHeaderCell>Actions
+              <CTableHeaderCell>
+                Actions
                 <CIcon icon={cilColorBorder} customClassName="nav-icon icon-small" />
               </CTableHeaderCell>
             </CTableRow>
           </CTableHead>
           <CTableBody>
-            {memberships.map((membership) => (<CTableRow key={membership?.id || ''}>
-              <CTableDataCell>{membership?.name || ''}</CTableDataCell>
-              <CTableDataCell>{membership?.duration || ''}</CTableDataCell>
-              <CTableDataCell>${membership?.price || ''}</CTableDataCell>
+            {memberships.map((membership) => (
+              <CTableRow key={membership?.id || ''}>
+                <CTableDataCell>{membership?.name || ''}</CTableDataCell>
+                <CTableDataCell>{membership?.duration || ''}</CTableDataCell>
+                <CTableDataCell>${membership?.price || ''}</CTableDataCell>
 
-              <CTableDataCell>
-                <CButton color="info" onClick={() => { setCurrentMembership(membership); setVisibleEdit(true); }} variant='outline' size="sm" className="me-2">Edit</CButton>
-
-                <CModal
-                  backdrop="static"
-                  visible={visibleEdit && currentMembership !== null}
-                  onClose={() => setVisibleEdit(false)}
-                  aria-labelledby="Modal Info"
-                >
-                  <CModalHeader>
-                    <CModalTitle id="Edit Membership">Edit Membership</CModalTitle>
-                  </CModalHeader>
-                  <CModalBody>
-                    <CRow className="mb-3">
-                      <CCol className='mb-3' md={6}>
-                        <CFormInput
-                          type="text"
-                          placeholder="Name"
-                          value={currentMembership?.name || ''}
-                          onChange={(e) => setCurrentMembership({ ...currentMembership, name: e.target.value })}
-                        />
-                      </CCol>
-                      <CCol className='mb-3' md={6}>
-                        <CFormInput
-                          type="number"
-                          placeholder="Duration"
-                          value={currentMembership?.duration || ''}
-                          onChange={(e) => setCurrentMembership({ ...currentMembership, duration: e.target.value })}
-                        />
-                      </CCol>
-                    </CRow>
-                    <CRow className="mb-3">
-                      <CCol className='mb-3' md={6}>
-                        <CFormInput
-                          type="number"
-                          placeholder="Cost"
-                          value={currentMembership?.price || ''}
-                          onChange={(e) => setCurrentMembership({ ...currentMembership, price: e.target.value })}
-                        />
-                      </CCol>
-                    </CRow>
-                  </CModalBody>
-                  <CModalFooter>
-                    <CButton color="secondary" onClick={() => setVisibleEdit(false)}>
-                      Close
-                    </CButton>
-                    <CButton color="primary" onClick={handleEditMembership}>Save Edit</CButton>
-                  </CModalFooter>
-
-                </CModal>
-                <CButton color="danger" onClick={() => { setCurrentMembership(membership); setVisibleDelete(true); }} variant='outline' size="sm">Delete</CButton> <CModal
-                  backdrop="static"
-                  visible={visibleDelete}
-                  onClose={() => setVisibleDelete(false)}
-                  aria-labelledby="Modal Info"
-                >
-                  <CModalHeader>
-                    <CModalTitle id="Create Users">Do you want delete?</CModalTitle>
-                  </CModalHeader>
-                  <CModalBody>
-                    <CRow className="mb-3">
-                      <label className='fw-bold mb-2'>Please write "confirm" if you want to delete this membership</label>
-                      <CCol className='mb-3' md={12}>
-                        <CForm>
+                <CTableDataCell>
+                  <CButton
+                    color="info"
+                    onClick={() => {
+                      setCurrentMembership(membership)
+                      setVisibleEdit(true)
+                    }}
+                    variant="outline"
+                    size="sm"
+                    className="me-2"
+                  >
+                    Edit
+                  </CButton>
+                  <CModal
+                    backdrop="static"
+                    visible={visibleEdit && currentMembership !== null}
+                    onClose={() => setVisibleEdit(false)}
+                    aria-labelledby="Modal Info"
+                  >
+                    <CModalHeader>
+                      <CModalTitle id="Edit Membership">Edit Membership</CModalTitle>
+                    </CModalHeader>
+                    <CModalBody>
+                      <CRow className="mb-3">
+                        <CCol className="mb-3" md={6}>
                           <CFormInput
                             type="text"
-                            id="Delete"
-                            value={deleteConfirmation}
-                            onChange={e => {
-                              setDeleteConfirmation(e.target.value);
-                              console.log(e.target.value); // Verifica el valor aquí
-                            }}
+                            placeholder="Name"
+                            value={currentMembership?.name || ''}
+                            onChange={(e) =>
+                              setCurrentMembership({ ...currentMembership, name: e.target.value })
+                            }
                           />
-                        </CForm>
-                      </CCol>
-                    </CRow>
-                  </CModalBody>
-                  <CModalFooter>
-                    <CButton color="secondary" onClick={() => setVisibleDelete(false)}>
-                      Close
-                    </CButton>
-                    <CButton color="primary" onClick={handleDeleteMembership}>Delete</CButton>
-                  </CModalFooter>
-                </CModal>
-              </CTableDataCell>
-            </CTableRow>
+                        </CCol>
+                        <CCol className="mb-3" md={6}>
+                          <CFormInput
+                            type="number"
+                            placeholder="Duration"
+                            value={currentMembership?.duration || ''}
+                            onChange={(e) =>
+                              setCurrentMembership({
+                                ...currentMembership,
+                                duration: e.target.value,
+                              })
+                            }
+                          />
+                        </CCol>
+                      </CRow>
+                      <CRow className="mb-3">
+                        <CCol className="mb-3" md={6}>
+                          <CFormInput
+                            type="number"
+                            placeholder="Cost"
+                            value={currentMembership?.price || ''}
+                            onChange={(e) =>
+                              setCurrentMembership({ ...currentMembership, price: e.target.value })
+                            }
+                          />
+                        </CCol>
+                      </CRow>
+                    </CModalBody>
+                    <CModalFooter>
+                      <CButton color="secondary" onClick={() => setVisibleEdit(false)}>
+                        Close
+                      </CButton>
+                      <CButton color="primary" onClick={handleEditMembership}>
+                        Save Edit
+                      </CButton>
+                    </CModalFooter>
+                  </CModal>
+                  <CButton
+                    color="danger"
+                    onClick={() => {
+                      setCurrentMembership(membership)
+                      setVisibleDelete(true)
+                    }}
+                    variant="outline"
+                    size="sm"
+                  >
+                    Delete
+                  </CButton>{' '}
+                  <CModal
+                    backdrop="static"
+                    visible={visibleDelete}
+                    onClose={() => setVisibleDelete(false)}
+                    aria-labelledby="Modal Info"
+                  >
+                    <CModalHeader>
+                      <CModalTitle id="Create Users">Do you want delete?</CModalTitle>
+                    </CModalHeader>
+                    <CModalBody>
+                      <CRow className="mb-3">
+                        <label className="fw-bold mb-2">
+                          Please write "confirm" if you want to delete this membership
+                        </label>
+                        <CCol className="mb-3" md={12}>
+                          <CForm>
+                            <CFormInput
+                              type="text"
+                              id="Delete"
+                              value={deleteConfirmation}
+                              onChange={(e) => {
+                                setDeleteConfirmation(e.target.value)
+                                console.log(e.target.value) // Verifica el valor aquí
+                              }}
+                            />
+                          </CForm>
+                        </CCol>
+                      </CRow>
+                    </CModalBody>
+                    <CModalFooter>
+                      <CButton color="secondary" onClick={() => setVisibleDelete(false)}>
+                        Close
+                      </CButton>
+                      <CButton color="primary" onClick={handleDeleteMembership}>
+                        Delete
+                      </CButton>
+                    </CModalFooter>
+                  </CModal>
+                </CTableDataCell>
+              </CTableRow>
             ))}
           </CTableBody>
         </CTable>
-      </CCardBody >
-    </CCard >
+      </CCardBody>
+    </CCard>
   )
 }
 export default Membership
