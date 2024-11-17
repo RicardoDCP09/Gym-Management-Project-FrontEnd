e832ebad0be477c9769b18ca3cf1b41cada8fcd7
Ricardo <154460778+RicardoDCP09@users.noreply.github.com>
2024-11-17 12:10:19 -0400

    Triying again

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,18 @@
 import { element } from 'prop-types'
 import React from 'react'
 
-const Dashboard = React.lazy(() => import('src/views/dashboard/Dashboard'))
-const Membership = React.lazy(() => import('src/views/pages/membership/membership.js'))
-const Classes = React.lazy(() => import('src/views/pages/classes_management/classes.js'))
-const User = React.lazy(() => import('src/views/pages/users/users.js'))
-const Inventory = React.lazy(() => import('src/views/pages/inventory/inventory.js'))
-const Payment = React.lazy(() => import('src/views/pages/payment/payment.js'))
-const Rutine = React.lazy(() => import('src/views/pages/rutine/rutine.js'))
-const Exercise = React.lazy(() => import('src/views/pages/exercise/exercise.js'))
-const Progress = React.lazy(() => import('src/views/pages/progress/progress.js'))
-const Reports = React.lazy(() => import('src/views/pages/reportss/report.js'))
-const Staff = React.lazy(() => import('src/views/pages/staff/staff.js'))
-const Login = React.lazy(() => import('src/views/pages/login/login.js'))
+const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
+const Membership = React.lazy(() => import('./views/pages/membership/membership.js'))
+const Classes = React.lazy(() => import('./views/pages/classes_management/classes.js'))
+const User = React.lazy(() => import('./views/pages/users/users.js'))
+const Inventory = React.lazy(() => import('./views/pages/inventory/inventory.js'))
+const Payment = React.lazy(() => import('./views/pages/payment/payment.js'))
+const Rutine = React.lazy(() => import('./views/pages/rutine/rutine.js'))
+const Exercise = React.lazy(() => import('./views/pages/exercise/exercise.js'))
+const Progress = React.lazy(() => import('./views/pages/progress/progress.js'))
+const Reports = React.lazy(() => import('./views/pages/reportss/report.js'))
+const Staff = React.lazy(() => import('./views/pages/staff/staff.js'))
+const Login = React.lazy(() => import('./views/pages/login/login.js'))
 
 const routes = [
   { path: '/', exact: true, name: 'Login', element: Login },
